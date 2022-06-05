fc542e4e01242be7e480a6de8525f2f729da37a0
Barnabé <39558923+yocbar@users.noreply.github.com>
2022-06-05 11:03:16 +0200

    Add files via upload

---
+++ socials.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Notre première instruction : echo</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        
+        <p>
+            Cette ligne a été écrite entièrement en HTML.<br />
+            <?php echo "Celle-ci a été écrite entièrement en PHP."; ?>
+        </p>
+    </body>
+</html>
